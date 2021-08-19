35ed74d64941b3a8368628321bfe6426ec3cc69b
WatermelonIce1 <86102261+WatermelonIce1@users.noreply.github.com>
2021-08-19 17:40:09 +0200

    Initial move to repository
    Moved files from old folder to repository folder, in order to publish

---
+++ WeatherappJS.js
@@ -0,0 +1,92 @@
+//Date Definition
+
+let now = new Date();
+
+let h5 = document.querySelector("h5");
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+minutes = minutes > 9 ? minutes : "0" + minutes;
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+h5.innerHTML = ` ${day}, ${month} ${date} ${year} ${hours}:${minutes} `;
+
+//searchCityresult
+
+function searchCity(event) {
+  event.preventDefault();
+  let apiKey = "bcce68a433d5a8c9240f3cdd82e592a0";
+  let searchInput = document.querySelector("#search-text-input");
+  let city = `${searchInput.value}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+let button = document.querySelector("#search");
+button.addEventListener("click", searchCity);
+console.log(searchCity);
+
+function currentPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "bcce68a433d5a8c9240f3cdd82e592a0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+
+//currentLocationplusTemp
+
+function showCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+let locateButton = document.querySelector("#currentLocation");
+locateButton.addEventListener("click", showCurrentPosition);
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let city = response.data.name;
+  let place = response.data.sys.country;
+  let condition = response.data.weather[0].description;
+  let humidity = response.data.main.humidity;
+
+  let temp = document.querySelector("#currentTemperature");
+  temp.innerHTML = `Current Temperature: ${temperature}˚C`;
+  let location = document.querySelector("#city");
+  location.innerHTML = `${city}, ${place}`;
+
+  let todaycondition = document.querySelector("#condition");
+  todaycondition.innerHTML = ` Condition: ${condition}`;
+  let todayhumidity = document.querySelector("#humidity");
+  todayhumidity.innerHTML = `Humidity: ${humidity}%`;
+
+  console.log(humidity);
+}
